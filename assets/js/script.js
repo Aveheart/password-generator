e230fd04db561013c116fdc9eba8aaa130127fdc
Averi <averi.sherman@gmail.com>
2022-08-20 00:03:20 -0400

    create way to make uppercase letters randomly

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,18 @@ var lowerCase;
 var upperCase;
 var number;
 var special;
+/*arrays for each varia*ble*/
+number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z"];
+special = ["*", "&", "^", "%", "$", "#", "!", "@", "+", "="];
 
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z"];
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
-var special = ["*", "&", "^", "%", "$", "#", "!", "@", "+", "="];
+/*make uppercase letters */
+var upper = [];
+var toUpper = function (x) {
+  return x.toUpperCase();
+}
+upperCase = lowerCase.map(toUpper);
+
 
 // Write password to the #password input
 function writePassword() {
@@ -23,3 +31,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  length = prompt("choose between 8 and 128 characters for password");
+  console.log("length of password" + length);
+}
